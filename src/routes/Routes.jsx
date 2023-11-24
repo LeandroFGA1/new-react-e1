b8885dddb6e7b74dde1973c91d93e0aa691c3e6d
LeandroFGA1 <aucache.leandro@gmail.com>
2023-11-24 19:43:36 -0300

    fix vercel v45

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Home from "../pages/home/Home"
-import About from '../pages/about/About.Jsx'
+import About from '../pages/about/About'
 import Store from '../pages/store/Store';
 
 function MainRoutes() {
